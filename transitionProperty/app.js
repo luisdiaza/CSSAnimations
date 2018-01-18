c1f3b6d2e6e2556775345c43d9896b38ad0b3ffd
Luis Diaz <diazluis1048@gmail.com>
2018-01-17 23:32:33 -0500

    created both class and hover implementations of transition property, added appropriate event listener, and edited README

--- transitionProperty/app.js
+++ transitionProperty/app.js
@@ -1,3 +1,9 @@
-$('.trigger').on('click', function(){
-  $(this).toggleClass('clicked');
+// $('.trigger').on('click', function(){
+//   $(this).toggleClass('clicked');
+// });
+
+const trigger = document.querySelector('.trigger');
+
+trigger.addEventListener('click',function(e){
+    trigger.classList.toggle('clicked');
 });
